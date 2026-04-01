44cdb4bc4a62488f7749ab25e48734cef5f0879a
Justus Tam <justustam23@gmail.com>
2026-04-01 17:48:07 -0400

    using char instead of String in CString

--- src/main/java/apcsa/githubtrack/CStringUtil.java
+++ src/main/java/apcsa/githubtrack/CStringUtil.java
@@ -6,4 +6,13 @@ public class CStringUtil
     {
         if ()
     }
+
+    public static int[] memeifyArray(int[] nums)
+    {
+        /*Returns a new array containing the same numbers in `nums`, but rearranged so that 
+        **every 6 is immediately followed by a 7**. Do not move the 6's, 
+        but every other number may move. The returned array must contain the same number of 6's and 7's, 
+        and every 6 should have a number after it that is not a 6. 7's may appear anywhere 
+        in the original array.*/
+    }
 }
