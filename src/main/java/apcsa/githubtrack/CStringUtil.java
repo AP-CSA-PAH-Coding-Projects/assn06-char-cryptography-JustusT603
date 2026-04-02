de88c1b98a1b536c0b5324cabaef8c742aeabe6c
Justus Tam <justustam23@gmail.com>
2026-04-01 22:35:13 -0400

    This is confusing

--- src/main/java/apcsa/githubtrack/CStringUtil.java
+++ src/main/java/apcsa/githubtrack/CStringUtil.java
@@ -37,7 +37,7 @@ public class CStringUtil
                 end--;
             }
         }
-        return true; 
+        return true; // if there are no opposite characters that are unequal, it is a palindrome
     }
 
     public static int[] toNumerical(CString str, int offset)
@@ -45,18 +45,109 @@ public class CStringUtil
         int[] nums = new int[str.getWord().length]; // Create an empty integer array the same size as str
         for (int i = 0; i < str.getWord().length; i++)
         {
-            nums[i] = str.getWord()[i] + offset; // 
+            nums[i] = str.getWord()[i] + offset; // the character is automatically casted into an int value
+            // the int value is then modified by the offset value
         }
         return nums;
     }
 
+    public static int maxMirror(CString str)
+    {
+        int maxLength = 0;
+        int currentLength = 0;
+
+        // Make an array of integers that represents the CString parameter
+        int[] numerical = toNumerical(str, 0);
+
+        // Loop through the original array
+        for (int i = 0; i < str.getWord().length; i++)
+        {
+            int j = i+1;
+            if (numerical[j] == numerical[i] + 1) // If adjacent values are ascending
+            {
+                currentLength++; // Increment the current run length
+                if (currentLength > maxLength) // If the current run length is greater than the max run length
+                {
+                    maxLength = currentLength; // Update the max run length
+                }
+            }
+            else if (numerical[j] == numerical[i] - 1) // If adjcaent values are descending
+            {
+                currentLength++; // Increment the current run length
+                if (currentLength> maxLength) // If the current run length is greater than the max run length
+                {
+                    maxLength = currentLength; // Update the max run length
+                }
+            }
+            else if (numerical[j] == numerical[i]) // If the adjcent values are equal,
+            {
+                if (numerical[j+1] == numerical[i]) // Check if they go back down (mirror)
+                {
+                    currentLength++; // Increment the current run length
+                    if (currentLength > maxLength) // If the current run length is greater than the max run length
+                    {
+                        maxLength = currentLength; // Update the max run length
+                    }
+                }
+            }
+        }
+
+        return maxLength;
+    }
+
+    public static int maxMirror(int[] nums)
+    {
+        int maxLength = 0;
+        int currentLength = 0;
+
+        // Loop through the original array
+        for (int i = 0; i < nums.length; i++)
+        {
+            int j = i+1;
+            if (nums[j] == nums[i] + 1) // If adjacent values are ascending
+            {
+                currentLength++; // Increment the current run length
+                if (currentLength > maxLength) // If the current run length is greater than the max run length
+                {
+                    maxLength = currentLength; // Update the max run length
+                }
+            }
+            else if (nums[j] == nums[i] - 1) // If adjcaent values are descending
+            {
+                currentLength++; // Increment the current run length
+                if (currentLength> maxLength) // If the current run length is greater than the max run length
+                {
+                    maxLength = currentLength; // Update the max run length
+                }
+            }
+            else if (nums[j] == nums[i]) // If the adjcent values are equal,
+            {
+                if (nums[j+1] == nums[i]) // Check if they go back down (mirror)
+                {
+                    currentLength++; // Increment the current run length
+                    if (currentLength > maxLength) // If the current run length is greater than the max run length
+                    {
+                        maxLength = currentLength; // Update the max run length
+                    }
+                }
+            }
+        }
+
+        return maxLength;
+    }
+
     public static int[] memeifyArray(int[] nums)
     {
-        /*Returns a new array containing the same numbers in `nums`, but rearranged so that 
-        **every 6 is immediately followed by a 7**. Do not move the 6's, 
-        but every other number may move. The returned array must contain the same number of 6's and 7's, 
-        and every 6 should have a number after it that is not a 6. 7's may appear anywhere 
-        in the original array.*/
+        
+    }
 
+    public static boolean nestedSequence(CString outer, CString inner)
+    {
+
+    }
+
+    public static CString decrypt(CString str)
+    {
+        
     }
 }
